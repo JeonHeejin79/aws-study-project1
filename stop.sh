e73d478f21d84bb1b9a6fa4a76343af7f4c40b2a
SKMNS\202110043 <jeonheejin@sk.com>
2024-03-11 19:47:06 +0900

    update

--- stop.sh
+++ stop.sh
@@ -6,7 +6,7 @@ sudo mkdir -vp /home/ubuntu/build/
 
 #sudo systemctl stop springserver
 PID=$(ps -ef | grep -v grep | grep 'java -jar /home/ubuntu/build/ROOT.war' | awk '{print $2}')
-if [ -n "$PID" ] then
+if [ -n "$PID" ]; then
 	kill -9 $PID
 	sleep 10
 fi
