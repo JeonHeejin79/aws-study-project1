25b06f5a3993c9c23e15e5f5a04ea9f3d5f7430a
SKMNS\202110043 <jeonheejin@sk.com>
2024-03-11 17:50:43 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,4 @@
-#cd /usr/local/tomcat
-#bin/shutdown.sh
-#cd webapps
-#rm -rf ROOT/
-#cp ~/build/test-app.war ./ROOT.war
-#cd ..
-#bin/startup.sh
-
-#APP_NAME= # 본인의 APP 이름
-#REPOSITORY=/home/ubuntu/build/
-
-#sudo systemctl start springservernohup
-#java -jar /home/ubuntu/build/ROOT.war 1>/dev/null 2>&1 &
+#!/bin/bash
 java -jar /home/ubuntu/build/ROOT.war
 
 
